760866fe77b8e5d844a9bb6fc232a8e7e23296c9
vpsx <19900057+vpsx@users.noreply.github.com>
2020-11-17 11:20:41 -0600

    Add test Shib-protected WSGI endpt; note REMOTE_USER

--- wsgi/src/main.py
+++ wsgi/src/main.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
+
+# Docs on how Shibboleth info is passed from server to application:
+# https://wiki.shibboleth.net/confluence/display/SP3/ApplicationIntegration
+# https://wiki.shibboleth.net/confluence/display/SP3/AttributeAccess#AttributeAccess-REMOTE_USER
 
 @app.route('/')
 def hello_world():
     return 'Hello, World! I\'m a Flask app!'
+
+
+@app.route('/apache')
+def apache():
+    # Inspect request context
+    print("request:")
+    print(request)
+    print(dir(request))
+    print(request.remote_user)
+    return "REMOTE_USER is..... " + request.remote_user
