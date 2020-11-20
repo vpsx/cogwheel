fd736add37d8138add98b713eb4543b55045f2c3
vpsx <19900057+vpsx@users.noreply.github.com>
2020-11-20 13:44:57 -0600

    Use Flask-SQLAlchemy

--- wsgi/src/main.py
+++ wsgi/src/main.py
@@ -21,45 +21,15 @@ def apache():
     return "REMOTE_USER is..... " + request.remote_user
 
 
-# SQLALCHEMY SETUP
-from sqlalchemy import create_engine
+from flask_sqlalchemy import SQLAlchemy
 # SQLite and in-memory for now...
-engine = create_engine('sqlite:///:memory:')#, echo=True)
-
-from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
-
-from sqlalchemy.orm import sessionmaker
-Session = sessionmaker(bind=engine)
-
-from contextlib import contextmanager
-
-@contextmanager
-def session_scope():
-    """Provide a transactional scope around a series of operations."""
-    """
-    def run_my_program():
-        with session_scope() as session:
-            ThingOne().go(session)
-            ThingTwo().go(session)
-    """
-    session = Session()
-    try:
-        yield session
-        session.commit()
-    except:
-        session.rollback()
-        raise
-    finally:
-        session.close()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+db = SQLAlchemy(app)
 
 
-from sqlalchemy import ForeignKey, Column, Integer, String
-from sqlalchemy.orm import relationship
-
-class User(Base):
+class User(db.Model):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     # other columns
 
     # TODO:
@@ -70,46 +40,46 @@ class User(Base):
     # For now, let's just stick the entire REMOTE_USER string into one column.
     # By the way, how do we get user attributes from the SAML assertion from here (here = the WSGI app)?
     # You will need those in order to get e.g. a sensible value for username.
-    shib_id = Column(String)
+    shib_id = db.Column(db.String)
 
     def get_user_id(self):
         return self.id
 
 from authlib.integrations.sqla_oauth2 import OAuth2ClientMixin
 
-class Client(Base, OAuth2ClientMixin):
+class Client(db.Model, OAuth2ClientMixin):
     __tablename__ = 'clients'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(
-        Integer, ForeignKey('users.id', ondelete='CASCADE')
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey('users.id', ondelete='CASCADE')
     )
-    user = relationship('User')
+    user = db.relationship('User')
 
 
 from authlib.integrations.sqla_oauth2 import OAuth2TokenMixin
 
-class Token(Base, OAuth2TokenMixin):
+class Token(db.Model, OAuth2TokenMixin):
     __tablename__ = 'tokens'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(
-        Integer, ForeignKey('users.id', ondelete='CASCADE')
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey('users.id', ondelete='CASCADE')
     )
-    user = relationship('User')
+    user = db.relationship('User')
 
 
 from authlib.integrations.sqla_oauth2 import OAuth2AuthorizationCodeMixin
 
-class AuthorizationCode(Base, OAuth2AuthorizationCodeMixin):
+class AuthorizationCode(db.Model, OAuth2AuthorizationCodeMixin):
     __tablename__ = 'authorization_codes'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(
-        Integer, ForeignKey('users.id', ondelete='CASCADE')
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey('users.id', ondelete='CASCADE')
     )
-    user = relationship('User')
+    user = db.relationship('User')
 
 
 # Make dem tables
-Base.metadata.create_all(engine)
+db.create_all()
 
 
 # Fake-register a test client...
@@ -123,14 +93,8 @@ test_client_metadata = {
         #, scope, token_endpoint_auth_method...
 }
 test_client.set_client_metadata(test_client_metadata)
-with session_scope() as session:
-    session.add(test_client)
-    session.commit()
-    print("Sanity-check test client details:")
-    print("id: " + str(test_client.id))
-    print("client_id: " + str(test_client.client_id))
-    print("redirect_uris: " + str(test_client.redirect_uris))
-    print("End sanity-check.")
+db.session.add(test_client)
+db.session.commit()
 # Done registering test client.
 
 
@@ -146,9 +110,8 @@ class AuthorizationCodeGrant(grants.AuthorizationCodeGrant):
             scope=request.scope,
             user_id=request.user.id,
         )
-        with session_scope() as session:
-            session.add(auth_code)
-            session.commit()
+        db.session.add(auth_code)
+        db.session.commit()
         return auth_code
 
     def query_authorization_code(self, code, client):
@@ -158,9 +121,8 @@ class AuthorizationCodeGrant(grants.AuthorizationCodeGrant):
             return item
 
     def delete_authorization_code(self, authorization_code):
-        with session_scope() as session:
-            session.delete(authorization_code)
-            session.commit()
+        db.session.delete(authorization_code)
+        db.session.commit()
 
     def authenticate_user(self, authorization_code):
         return User.query.get(authorization_code.user_id)
@@ -171,10 +133,8 @@ from authlib.integrations.sqla_oauth2 import (
     create_query_client_func,
     create_save_token_func
 )
-query_client = create_query_client_func(Session(), Client)
-save_token = create_save_token_func(Session(), Token)
-#query_client = create_query_client_func(db.session, Client)
-#save_token = create_save_token_func(db.session, Token)
+query_client = create_query_client_func(db.session, Client)
+save_token = create_save_token_func(db.session, Token)
 
 server = AuthorizationServer(
     app, query_client=query_client, save_token=save_token
@@ -187,16 +147,16 @@ server.register_grant(AuthorizationCodeGrant)
 from flask import request, render_template
 #from your_project.auth import current_user
 
-def get_or_create_shib_user(dbsession):
+def get_or_create_shib_user():
     if request.remote_user:
-
-        user = dbsession.query(User).filter(User.shib_id == request.remote_user).one_or_none()
+        user = User.query.filter(User.shib_id == request.remote_user).one_or_none()
         if not user:
+            print("User not found in db; creating...")
             new_user = User(shib_id=request.remote_user)
-            dbsession.add(new_user)
-            dbsession.commit()
+            db.session.add(new_user)
+            db.session.commit()
+            print("User created.")
             user = new_user
-
         return user
     return "No value for request.remote_user--is this being called outside a request context?"
 
@@ -207,26 +167,22 @@ def authorize():
     # It can be done with a redirection to the login page, or a login
     # form on this authorization page.
 
-    with session_scope() as s:
+    current_user = get_or_create_shib_user()
 
-        # What happens if current_user is just a string?
-        #current_user = "Lactobacillus bulgaricus"
-        #current_user = get_or_create_shib_user()
-        current_user = get_or_create_shib_user(s)
+    if request.method == 'GET':
+        grant = server.validate_consent_request(end_user=current_user)
+        return render_template(
+            'authorize.html',
+            grant=grant,
+            user=current_user,
+        )
+    confirmed = request.form['confirm']
+    if confirmed:
+        # granted by resource owner
+        return server.create_authorization_response(grant_user=current_user)
+    # denied by resource owner
+    return server.create_authorization_response(grant_user=None)
 
-        if request.method == 'GET':
-            grant = server.validate_consent_request(end_user=current_user)
-            return render_template(
-                'authorize.html',
-                grant=grant,
-                user=current_user,
-            )
-        confirmed = request.form['confirm']
-        if confirmed:
-            # granted by resource owner
-            return server.create_authorization_response(grant_user=current_user)
-        # denied by resource owner
-        return server.create_authorization_response(grant_user=None)
 
 @app.route('/oauth/token', methods=['POST'])
 def issue_token():
