ebed2def5e2e05830120ca102ada74c94fb68a4b
vpsx <19900057+vpsx@users.noreply.github.com>
2020-12-23 16:47:53 -0600

    Silence warning about SQLALCHEMY_TRACK_MODIFICATIONS

--- wsgi/src/default_settings.py
+++ wsgi/src/default_settings.py
@@ -1,4 +1,10 @@
-DEBUG = False
 # SQLite and in-memory for now...
 SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+
 PRIVATE_KEY_PATH = '/var/www/wsgi/privatekey.pem'
+
+
+DEBUG = False
+
+# Disabled by default; setting here to silence warning
+SQLALCHEMY_TRACK_MODIFICATIONS = False
